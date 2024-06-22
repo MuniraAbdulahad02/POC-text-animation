d03c6eacbb5d45cd48a295aeb090107db80ac60a
muniraabdulahad <m.abdulahad@student.fontys.nl>
2024-06-22 20:00:59 +0200

    Aditional animation added ScrollTrigger

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { gsap } from "gsap";
-import './style.css';
+import { ScrollTrigger } from "gsap/ScrollTrigger"; // Import ScrollTrigger for scrolling animations
+
+// import './style.css';
 
 // Immediate action
 // gsap.set
@@ -14,6 +16,14 @@ import './style.css';
 // X,Y -> moving an object
 // Scale -> scaling an object 
 
+const $text = document.getElementById("title");
+const $use = document.getElementById("use");
+
+gsap.set([$text, $use], { alpha: 0.0, y: 10.0 });
+
+gsap.to([$text, $use], { alpha: 1.0, ease: "none", duration: 1.0 });
+gsap.to([$text, $use], { y: 0.0, ease: "ease.out", duration: 1.5 });
+
 const $name = document.getElementById("name")
 const $portfolio = document.getElementById("portfolio")
 const $button = document.getElementById("button")
@@ -26,3 +36,32 @@ const handleClick = () => {
 };
 
 $button.addEventListener("click", handleClick);
+
+gsap.registerPlugin(ScrollTrigger);
+
+const $additionalText = document.getElementById("additional-text");
+
+gsap.set($additionalText, { opacity: 0, y: 20 });
+
+
+function triggerAnimation(element) {
+  // Animation when scrolling triggers the section
+  gsap.to(element, {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: element, // Section that triggers the animation
+      ease: "power2.out", // Easing effect
+    },
+  });
+}
+
+triggerAnimation("#additional-text")
+
+// const listID = ["#additional-text"]
+
+// listID.forEach((id) => {
+//   triggerAnimation(id);
+// })
+
